3a587c5d6c78b9cf7f7fb1619e46f4e58e368807
helio <helio.de-breyne@laplateforme.io>
2019-11-08 20:38:26 +0100

    jour01

---
+++ jour01/Job01/index.php
@@ -0,0 +1,15 @@
+<?php
+ $str = "LaPlateforme";
+ $str2 = "Vive";
+ $str3 = "!";
+ echo "$str2 $str $str3";
+ $val = 6;
+ echo "$val";
+ echo "<br>";
+ $val2 = 4;
+ echo $val+$val2;
+ $myBool = true;
+ echo "$myBool";
+ $myBool2 = false;
+ echo "$myBool2";
+?>
